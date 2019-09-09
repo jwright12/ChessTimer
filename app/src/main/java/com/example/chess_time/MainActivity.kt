a3509f0bbb6799b6c663c669c5f2670332b58f8c
Joseph Wright <jwright@Josephs-MacBook-Pro.local>
2019-09-09 18:14:58 -0500

    Created view for a single match card for use in recycler view

--- app/src/main/java/com/example/chess_time/MainActivity.kt
+++ app/src/main/java/com/example/chess_time/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.example.chess_time
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
-import kotlinx.android.synthetic.main.content_main.*
-import android.os.CountDownTimer
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Android will destroy the view when the activity is not needed anymore
+        // Adds capability to persist orientation and life cycle changes, we're passing a class using livedata
         timerViewModel = ViewModelProviders.of(this).get(TimerViewModel::class.java)
 
         // This observer manages live data through life cycle changes and more
