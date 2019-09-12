35ec80db25bd28713c001f0abb6a4f5445d6f508
Joseph Wright <jwright12@winona.edu>
2019-09-12 08:39:05 -0500

    Adapter to manage data for the rows in a recycler view

--- app/src/main/java/com/example/chess_time/TimerAdapter.kt
+++ app/src/main/java/com/example/chess_time/TimerAdapter.kt
@@ -3,40 +3,54 @@ package com.example.chess_time
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.timer_item.view.*
 
-class TimerAdapter : ListAdapter<Timer, TimerAdapter.TimerHolder> (){
+class TimerAdapter : ListAdapter<Timer, TimerAdapter.TimerHolder> (DIFF_CALLBACK){
 
     companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Timer>() {
+            override fun areItemsTheSame(oldItem: Timer, newItem: Timer): Boolean {
+                return oldItem.id == newItem.id
+            }
 
+            override fun areContentsTheSame(oldItem: Timer, newItem: Timer): Boolean {
+                return oldItem.title == newItem.title
+            }
+        }
     }
 
-    private var listener: AdapterView.OnItemClickListener? = null
-
-    override fun onBindViewHolder(holder: TimerHolder, position: Int) {
-        val currentTimer: Timer = getItem(position)
-
-        holder.textViewMatchTitle.text = currentTimer.
-
-    }
-
-
-    fun getTimerAt(position: Int) : Timer {
-        return getItem(position)
-    }
-
+    private var listener: OnItemClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimerHolder {
         val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.timer_item, parent, false)
         return TimerHolder(itemView)
     }
 
+    override fun onBindViewHolder(holder: TimerHolder, position: Int) {
+        val currentTimer: Timer = getItem(position)
 
+        holder.textViewMatchTitle.text = currentTimer.title
+        holder.textViewMatchType.text = currentTimer.Type
+        holder.textViewWhiteDuration.text = currentTimer.durationWhite.toString()
+        holder.textViewBlackDuration.text = currentTimer.durationBlack.toString()
+        holder.textViewWhiteIncrement.text = currentTimer.incrementWhite.toString()
+        holder.textViewBlackIncrement.text = currentTimer.incrementBlack.toString()
+        holder.textViewWhiteDelay.text = currentTimer.delayWhite.toString()
+        holder.textViewBlackDelay.text = currentTimer.delayBlack.toString()
+    }
+
+    fun getTimerAt(position: Int): Timer {
+        return getItem(position)
+    }
+
+
+    // Holds a reference to each item in a recycler view
     inner class TimerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         init {
             itemView.setOnClickListener {
                 val position = adapterPosition
@@ -56,7 +70,6 @@ class TimerAdapter : ListAdapter<Timer, TimerAdapter.TimerHolder> (){
         var textViewWhiteDelay: TextView = itemView.white_delay_text_view
         var textViewBlackDelay: TextView = itemView.black_delay_text_view
 
-
     }
 
     interface OnItemClickListener {
@@ -66,6 +79,4 @@ class TimerAdapter : ListAdapter<Timer, TimerAdapter.TimerHolder> (){
     fun setOnItemClickListener(listener: OnItemClickListener) {
         this.listener = listener
     }
-
-
 }
