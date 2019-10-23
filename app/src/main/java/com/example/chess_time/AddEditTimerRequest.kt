d0a838db677f5d7de5e55be78832f691c0efbd81
Joseph Wright <jwright12@winona.edu>
2019-10-23 18:43:33 +0200

    Writing logic to populate DB with new data

--- app/src/main/java/com/example/chess_time/AddEditTimerRequest.kt
+++ app/src/main/java/com/example/chess_time/AddEditTimerRequest.kt
@@ -1,10 +1,13 @@
 package com.example.chess_time
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_edit_timer.*
+import kotlinx.android.synthetic.main.timer_item.*
 
 class AddEditTimerRequest : AppCompatActivity() {
 
@@ -17,8 +20,6 @@ class AddEditTimerRequest : AppCompatActivity() {
         const val EXTRA_INC_BLACK = "com.example.chess_time.EXTRA_INC_BLACK"
         const val EXTRA_DELAY_WHITE = "com.example.chess_time.EXTRA_DELAY_WHITE"
         const val EXTRA_DELAY_BLACK = "com.example.chess_time.EXTRA_DELAY_BLACK"
-        const val EXTRA_TYPE = "com.example.chess_time.EXTRA_TYPE"
-        const val EXTRA_PLAY_COUNT = "com.example.chess_time.EXTRA_PLAY_COUNT"
     }
 
 
@@ -27,12 +28,44 @@ class AddEditTimerRequest : AppCompatActivity() {
         setContentView(R.layout.add_edit_timer)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
 
+        if (intent.hasExtra(EXTRA_ID)) {
+            // If the intent was already assigned an ID, we know we are trying to edit an existing timer
+            title = "Edit Match Settings"
+            title_text.setText(intent.getStringExtra(EXTRA_TITLE))
+
+            white_duration_edit_text.setText(intent.getStringExtra(EXTRA_TIME_WHITE))
+            black_duration_edit_text.setText(intent.getStringExtra(EXTRA_TIME_BLACK))
+
+            white_increment_edit_text.setText(intent.getStringExtra(EXTRA_INC_WHITE))
+            black_increment_edit_text.setText(intent.getStringExtra(EXTRA_INC_BLACK))
+
+            white_delay_edit_text.setText(intent.getStringExtra(EXTRA_DELAY_WHITE))
+            black_delay_edit_text.setText(intent.getStringExtra(EXTRA_DELAY_BLACK))
+
+        } else {
+            title = "Create New Match"
+        }
+
+
+
 
     }
 
     private fun saveTimer() {
 
-        
+        // If title or duration has no entry, then prevent the save
+        if (title_text.text.toString().trim().isBlank()) {
+            Toast.makeText(this, "Enter a title before saving", Toast.LENGTH_SHORT).show()
+            return
+
+        } else if (white_duration_edit_text.text.toString().trim().isBlank() && black_duration_edit_text.text.toString().trim().isBlank()) {
+            Toast.makeText(this, "Provide the match duration", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val data = Intent().apply {  }
+
+
     }
 
 
